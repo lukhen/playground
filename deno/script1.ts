03c9f082ebd9dd9db2c95190fe8b7081a99f30de
lukhen <lukasz.hen@gmail.com>
2022-06-01 10:24:43 +0200

    Testing a simple async function in deno.

--- deno/script1.ts
+++ deno/script1.ts
@@ -1,7 +1,23 @@
 import * as E from "https://deno.land/x/fp_ts@v2.11.4/Either.ts"
-import { assertEquals } from "https://deno.land/std@0.141.0/testing/asserts.ts"
+import * as TE from "https://deno.land/x/fp_ts@v2.11.4/TaskEither.ts"
+
+import { assertEquals, assert } from "https://deno.land/std@0.141.0/testing/asserts.ts"
 
 const x: E.Either<number, string> = E.right("qw")
+
+interface User {
+    name: string
+}
+
+const f:
+    (n: number) => TE.TaskEither<string, User> =
+    n => TE.right({ name: "lukhen" })
+
+
+Deno.test("Some test", async () => {
+    const x = await f(2)()
+    assert(E.isRight(x), "fetch failed")
+})
 
 Deno.test("First deno test", () => {
     assertEquals(x, E.right("qw"), "failed because of some reason")
