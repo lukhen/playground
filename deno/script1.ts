72b6d8292e7aa0c7f3775ba600f4c547fb78b19a
lukhen <lukasz.hen@gmail.com>
2022-06-01 08:34:05 +0200

    First test in deno.

--- deno/script1.ts
+++ deno/script1.ts
@@ -1,5 +1,9 @@
 import * as E from "https://deno.land/x/fp_ts@v2.11.4/Either.ts"
+import { assertEquals } from "https://deno.land/std@0.141.0/testing/asserts.ts"
 
 const x: E.Either<number, string> = E.right("qw")
 
-console.log(x)
+Deno.test("First deno test", () => {
+    assertEquals(x, E.right("qw"), "failed because of some reason")
+})
+
